e2ac156c6d545f622786f20f0941db35a1ddf9db
Arvis Abromovics <info@vsmedio.lv>
2018-01-10 23:56:21 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Статики-2
+1. В статическом блоке считайте две переменные с консоли А и В с типом int.
+2. Не забыть про IOException, который надо обработать в блоке catch.
+3. Закрыть поток ввода методом close().
+
+
+Требования:
+1. Поле A должно быть статическим.
+2. Поле B должно быть статическим.
+3. Программа должна считывать данные с клавиатуры в статическом блоке.
+4. Программа должна инициализировать поля A и B в статическом блоке согласно введенным с клавиатуры значениям.
+5. Программа должна выводить в консоль минимальное из введенных с клавиатуры значений.
+*/
+
+public class Solution {
+    public static int A;
+    public static int B;
+
+    static {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            A = Integer.parseInt(reader.readLine());
+            B = Integer.parseInt(reader.readLine());
+            reader.close();
+        } catch (IOException e) {
+            System.out.println ("ERROR:"+e.getMessage());
+        }
+    }
+
+    public static final int MIN = min(A, B);
+
+    public static void main(String[] args) {
+        System.out.println(MIN);
+    }
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+}
